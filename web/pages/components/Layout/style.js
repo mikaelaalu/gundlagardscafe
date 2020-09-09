e9ffc2a447d8c5cb1754abb49c64feceaceb7ca0
Mikaela <mikaelalundsgard@hotmail.com>
2020-09-09 11:23:23 +0200

    styled layout

--- web/pages/components/Layout/style.js
+++ web/pages/components/Layout/style.js
@@ -4,6 +4,10 @@ const wrapper = styled.section`
   width: 100%;
   display: flex;
   justify-content: space-between;
+  height: 103px;
+  background-color: var(--dipp-beige);
+  position: sticky;
+  top: 0;
 `;
 
 export default { wrapper };
