5f9832bdb88872731db079679bd394999342b111
Camilla <cks@live.se>
2020-09-10 13:44:22 +0200

    update InstagramFeed component
    narrowed down to six images

--- web/pages/components/InstagramFeed/index.js
+++ web/pages/components/InstagramFeed/index.js
@@ -1,10 +1,11 @@
 const InstagramFeed = ({ props }) => {
-  const images = props.graphql.user.edge_owner_to_timeline_media.edges;
+  const allImages = props.graphql.user.edge_owner_to_timeline_media.edges;
+  const sixImages = allImages.slice(0, 6);
 
   return (
     <div>
       <h2>Wow this is cool, it's a instagramfeed!!</h2>
-      {images.map((image, i) => (
+      {sixImages.map((image, i) => (
         <div key={i}>
           <img width={"200px"} src={image.node.display_url} key={i}></img>
         </div>
