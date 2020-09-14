15ecc9665924cf3e868491dfe9170347f43d8708
Camilla <cks@live.se>
2020-09-14 14:03:54 +0200

    update menu and about page

--- web/pages/about.js
+++ web/pages/about.js
@@ -8,13 +8,10 @@ import imageUrlBuilder from "../imageUrlBuilder";
 const AboutPage = (props) => {
   console.log("about page:");
   console.log(props);
-  const IconSrc = props.icon[0].iconArray[1].iconImage.asset._ref;
-  const IconAlt = props.icon[0].iconArray[1].iconAlt;
 
   return (
     <Layout props={props}>
-      <PageIcon src={imageUrlBuilder(IconSrc)} alt={IconAlt} />
-      <About props={props.about[0]} />
+      <About about={props.about[0]} icon={props.icon[0]} /> 
     </Layout>
   );
 };
