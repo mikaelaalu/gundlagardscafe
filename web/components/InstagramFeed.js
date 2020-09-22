2a9f5676f0a59a0998cd390344b384c5918a83e6
Mikaela <mikaelalundsgard@hotmail.com>
2020-09-22 14:14:21 +0200

    update media queries on startpage

--- web/components/InstagramFeed.js
+++ web/components/InstagramFeed.js
@@ -1,6 +1,10 @@
 import styled from "styled-components";
 const Title = styled.h3`
   padding-left: 21px;
+  @media (min-width: 768px) {
+    padding-left: 5px;
+    width: 70vw;
+  }
 `;
 
 const ImgWrapper = styled.div`
@@ -8,29 +12,48 @@ const ImgWrapper = styled.div`
   width: 100vw;
   padding-bottom: 85px;
   padding-top: 24px;
+  @media (min-width: 768px) {
+    align-items: center;
+    justify-content: center;
+    width: 70vw;
+  }
 `;
 
 const Img = styled.img`
   width: 100%;
-  height: 100%;
+  height: auto;
   padding: 5px;
+`;
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+const InstaSection = styled.section`
+  @media (min-width: 768px) {
+    width: 100vw;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
 `;
 
 const InstagramFeed = ({ props, title }) => {
   const fourImages = props.slice(0, 4);
 
   return (
-    <section>
+    <InstaSection>
       <Title>{title}</Title>
 
       <ImgWrapper>
         {fourImages.map((image, i) => (
-          <div key={i}>
+          <Container key={i}>
             <Img src={image.node.display_url} key={i} />
-          </div>
+          </Container>
         ))}
       </ImgWrapper>
-    </section>
+    </InstaSection>
   );
 };
 
